41c33f05696dd2d203b4326564e31a0ab78b35ca
CameronMorrison97 <cameron.morrison1997@gmail.com>
2019-03-14 22:29:31 +0000

    got rid of cout and fixed a bug

--- locking/locking.cpp
+++ locking/locking.cpp
@@ -6,16 +6,18 @@
 #include <thread>
 #include "account.h"
 #include <vector>
+#include <mutex>
 
 // Import things we need from the standard library
 using std::cout;
 using std::endl;
 using std::thread;
 using std::vector;
+using std::mutex;
 
 Account bill;
 
-void bills(Account bill) 
+void bills() 
 {
 	for (int stephen = 0; stephen < 1000000; stephen++)
 	{
@@ -29,20 +31,15 @@ int main(int argc, char *argv[])
 	cout << "Initial: " << bill.total() << "\n";
 
 	vector<thread> billThreads;
-	billThreads.resize(10);
 	for (int i = 0; i < 10; i++)
 	{
-		thread luke(bills, bill);
-		billThreads.push_back(luke);
-	}
-	
-	for (int i = 0; i < 10; i++)
-	{
-		billThreads[i].join();
+		billThreads.push_back(std::thread(bills));
 	}
 
-	cout << "Cake is fucking delicious\n\n";
-		
+
+	for (int i = 0; i < billThreads.size(); i++) {
+		billThreads[i].join();
+	}
 
 	cout << "Total: " << bill.total() << "\n";
 
